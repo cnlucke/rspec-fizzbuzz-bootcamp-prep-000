1c9e84920874e53d21abcf9582452492aa55df6b
Cristy Lucke <cristy.lucke@gmail.com>
2017-10-15 01:18:18 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
-def fizzbuzz(number)
-  if ((number % 3) == 0) && ((number % 5) == 0)
+def fizzbuzz(num)
+  if ((num % 3) == 0) && ((num % 5) == 0)
     return "Fizzbuzz"
-  elsif ((number % 3) == 0)
+  elsif ((num % 3) == 0)
     return "Fizz"
-  elsif ((number % 5) == 0)
+  elsif ((num % 5) == 0)
     return "Buzz"
   else
     return nil
