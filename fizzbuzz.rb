5fef01cd44a7aaff571f4e8fdc1f184c50fbdc2f
Cristy Lucke <cristy.lucke@gmail.com>
2017-10-15 01:03:31 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,10 @@
+def fizzbuzz
+  if (number / 3) == 0
+    return "Fizz"
+  elsif (number / 5) == 0
+    return "Buzz"
+  elsif ((number / 3) == 0) && ((number / 5) == 0)
+    return "Fizzbuzz"
+  else
+    return nil
+end
